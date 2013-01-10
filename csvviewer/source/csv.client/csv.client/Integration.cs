f2ab38c907fb7f650a3a154fa674bd0b3de8f8b2
Ralf Westphal <info@ralfw.de>
2013-01-10 15:28:24 +0100

    Client auf Inkrement 2 eingestellt

--- csvviewer/source/csv.client/csv.client/Integration.cs
+++ csvviewer/source/csv.client/csv.client/Integration.cs
@@ -17,13 +17,17 @@ namespace csv.client
             _viewer = viewer;
             _file = file;
             _logik = logik;
+
+            _viewer.Nächste_Seite += Next;
         }
+
 
         public void Starten()
         {
             Zeige_erste_Seite();
             _viewer.Menü_anzeigen();
         }
+
 
         void Zeige_erste_Seite()
         {
@@ -32,11 +36,39 @@ namespace csv.client
             _viewer.Zeige_Seite_an(formatierte_Seite);
         }
 
+        void Next()
+        {
+            var rohe_nächste_Seite = Hole_Records_nächste_Seite();
+            var formatierte_Seite = _logik.Formatiere_Seite(rohe_nächste_Seite);
+            _viewer.Zeige_Seite_an(formatierte_Seite);
+        }
+
+
         Tuple<string,IEnumerable<string>> Hole_Records_erste_Seite()
         {
             var dateiname = _starter.Dateiname_von_Cmdline_lesen();
             var rohe_zeilen = _file.Alle_Zeilen_laden(dateiname);
-            return _logik.Erste_Seite_entnehmen(rohe_zeilen);
+            return Erste_Seite_entnehmen(rohe_zeilen);
+        }
+
+        private Tuple<string, IEnumerable<string>> Hole_Records_nächste_Seite()
+        {
+            var dateiname = _starter.Dateiname_von_Cmdline_lesen();
+            var rohe_zeilen = _file.Alle_Zeilen_laden(dateiname);
+            return Nächste_Seite_entnehmen(rohe_zeilen);
+        }
+
+
+        Tuple<string,IEnumerable<string>> Erste_Seite_entnehmen(string[] rohe_zeilen)
+        {
+            var seiteninfo = _logik.Ermittle_erste_Seitennummer(rohe_zeilen);
+            return _logik.Entnehme_Seite(seiteninfo);
+        }
+    
+        Tuple<string, IEnumerable<string>> Nächste_Seite_entnehmen(string[] rohe_zeilen)
+        {
+            var seiteninfo = _logik.Ermittle_nächste_Seitennummer(rohe_zeilen);
+            return _logik.Entnehme_Seite(seiteninfo);
         }
     }
 }
