9225a7028e6c5c06d389c6aa1da2e952e9123694
David Schäfer <david.schaefer@analytical-software.de>
2013-01-10 17:45:42 +0100

    ToDictionary von David

---
+++ david/AgileSoftwarearchitektur/TestToDictionary/TestToDictionary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDictionary;
+
+namespace TestToDictionary
+{
+    [TestClass]
+    public class TestToDictionary
+    {
+        [TestMethod]
+        public void EverythingIsFineTest()
+        {
+            string config = "a=1;b=hallo;c=david schäfer";
+            var expectedObject = new Dictionary<string, string> {{"a", "1"}, {"b", "hallo"}, {"c", "david schäfer"}};
+
+            var todicitonary = new ToDictionary.ToDictionary();
+            var actualObject = todicitonary.ToDictionaryMethod(config);
+
+            CollectionAssert.AreEquivalent(expectedObject.Keys, actualObject.Keys);
+            CollectionAssert.AreEquivalent(expectedObject.Values, actualObject.Values);
+        }
+
+        [TestMethod]
+        public void InputNullTest()
+        {
+            string config = null;
+            bool errorOccur = false;
+            try
+            {
+                var todicitonary = new ToDictionary.ToDictionary();
+                todicitonary.ToDictionaryMethod(config);
+            }
+            catch (Exception)
+            {
+                errorOccur = true;
+            }
+            Assert.IsTrue(errorOccur);
+        }
+    }
+}
---
+++ david/AgileSoftwarearchitektur/ToDictionary/ToDictionary.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace ToDictionary
+{
+    public class ToDictionary
+    {
+        public Dictionary<string, string> ToDictionaryMethod(string config)
+        {
+            List<string> stringList = SplitString(config);
+            return ConvertStringListToDictionary(stringList);
+        }
+
+        public List<string> SplitString(string config)
+        {
+            if (config == null)
+                throw new Exception("Input is null");
+
+            return config.Split(';').ToList();
+        }
+
+        public Dictionary<string, string> ConvertStringListToDictionary(List<string> stringList)
+        {
+            var intermediate = SplitStringListToKeyValue(stringList);
+            return AddKeyValuesToDictionary(intermediate);
+        }
+
+        public List<KeyValuePair<string,string>> SplitStringListToKeyValue(List<string> stringList)
+        {
+            var resultList = new List<KeyValuePair<string, string>>();
+            foreach (string stringValue in stringList)
+            {
+                if (stringValue.Contains("="))
+                {
+                    string key = stringValue.Substring(0, stringValue.IndexOf("=", StringComparison.Ordinal));
+                    string value = stringValue.Substring(stringValue.IndexOf("=", StringComparison.Ordinal) + 1);
+                    resultList.Add(new KeyValuePair<string, string>(key, value));
+                }
+                else
+                {
+                    resultList.Add(new KeyValuePair<string, string>(stringValue,null));
+                }
+            }
+            return resultList;
+        }
+
+        public Dictionary<string,string> AddKeyValuesToDictionary(List<KeyValuePair<string,string>> listOfKeyValues)
+        {
+            var result = new Dictionary<string, string>();
+            foreach (KeyValuePair<string, string> keyValuePair in listOfKeyValues)
+            {
+                result[keyValuePair.Key] =keyValuePair.Value;    
+            }
+            return result;
+        }
+
+
+
+
+    }
+}
