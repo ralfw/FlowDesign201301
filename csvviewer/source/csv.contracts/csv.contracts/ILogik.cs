b6843c2a90ddc4bd80e63802b5cba53e77fb9e51
Ralf Westphal <info@ralfw.de>
2013-01-10 14:26:02 +0100

    Kontrakte für Iteration 2

--- csvviewer/source/csv.contracts/csv.contracts/ILogik.cs
+++ csvviewer/source/csv.contracts/csv.contracts/ILogik.cs
@@ -5,7 +5,10 @@ namespace csv.contracts
 {
     public interface ILogik
     {
-        Tuple<string, IEnumerable<string>> Erste_Seite_entnehmen(string[] rohzeilen);
+        Tuple<string, IEnumerable<string>> Entnehme_Seite(Tuple<int,int,string[]> seiteninfo);
+        Tuple<int, int, string[]> Ermittle_erste_Seitennummer(string[] rohzeilen);
+        Tuple<int, int, string[]> Ermittle_nächste_Seitennummer(string[] rohzeilen);
+
         IEnumerable<string> Formatiere_Seite(Tuple<string, IEnumerable<string>> rohseite);
     }
 }
