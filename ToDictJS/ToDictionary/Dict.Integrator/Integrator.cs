54da02eea49dec51db1a95b0ed44c4761d372bb7
Johannes Schluchter <johannes.schluchter@analytical-software.de>
2013-01-10 17:41:01 +0100

    ToDictionary von JS

---
+++ ToDictJS/ToDictionary/Dict.Integrator/Integrator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ToDictionary;
+
+namespace Dict.Integrator
+{
+    public class Integrator
+    {
+        public Dictionary<string, string> ToDictionary(string config)
+        {
+            var logic = new DictionaryCreatorLogic();
+            var entries = logic.CreateEntries(config);
+            var kvPairs = logic.CreateKeyValuePairs(entries);
+
+            return logic.CreateDictionaryFromKeyValuePairs(kvPairs);
+        }
+    }
+}
---
+++ ToDictJS/ToDictionary/ToDictionary/DictionaryCreatorLogic.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ToDictionary
+{
+    public class DictionaryCreatorLogic
+    {
+        public List<string> CreateEntries(string config)
+        {
+            List<string> entries = config.Split(';').ToList();
+            List<string> result = new List<string>();
+
+            foreach (var entry in entries)
+            {
+                if(string.IsNullOrEmpty(entry))
+                {
+                    continue;
+                }
+
+                result.Add(entry);
+            }
+
+            return result;
+        }
+
+        public List<Tuple<string, string>> CreateKeyValuePairs(IEnumerable<string> entries)
+        {
+            List<Tuple<string, string>> keyValuePairs = new List<Tuple<string, string>>();
+
+            foreach (var entry in entries)
+            {
+                string[] kvPair = entry.Split('=');
+                string key = kvPair[0];
+                string value = (kvPair.Length == 2) ? kvPair[1] :"";
+
+                keyValuePairs.Add(new Tuple<string, string>(key, value));
+            }
+
+            return keyValuePairs;
+        }
+
+        public Dictionary<string, string> CreateDictionaryFromKeyValuePairs(List<Tuple<string, string>> kvPairs)
+        {
+            Dictionary<string, string> result = new Dictionary<string, string>();
+
+            foreach (var kvPair in kvPairs)
+            {
+                result[kvPair.Item1] = kvPair.Item2;
+            }
+
+            return result;
+        }
+    }
+}
